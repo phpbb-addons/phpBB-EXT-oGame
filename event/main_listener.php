1d8f1c73b8482a7d3274d7ffc4b20a834570cc15
Falk Seidel <Un1matr1x@users.noreply.github.com>
2015-01-23 21:12:58 +0100

    [WIP] rewritte the search-pattern and the replacement

--- event/main_listener.php
+++ event/main_listener.php
@@ -56,15 +56,15 @@ class main_listener implements EventSubscriberInterface
 	/**
 	 * Constructor
 	 *
-	 * @param string									$php_ext
+	 * @param string								$php_ext
 	 * @param Container								$phpbb_container
-	 * @param \phpbb\db\driver\driver_interfacer		$db
+	 * @param \phpbb\db\driver\driver_interfacer	$db
 	 * @param \phpbb\config\config					$config
 	 * @param \phpbb\controller\helper				$helper		Controller helper object
 	 * @param \phpbb\auth\auth						$auth
 	 * @param \phpbb\template						$template	Template object
 	 * @param \phpbb\user							$user
-	 * @param \phpbb\path_helper						$phpbb_path_helper
+	 * @param \phpbb\path_helper					$phpbb_path_helper
 	 * @param \phpbb\extension\manager				$phpbb_extension_manager
 	 */
 	public function __construct($php_ext, Container $phpbb_container, \phpbb\db\driver\driver_interface $db,
@@ -73,15 +73,15 @@ class main_listener implements EventSubscriberInterface
 								\phpbb\user $user, \phpbb\path_helper $phpbb_path_helper,
 								\phpbb\extension\manager $phpbb_extension_manager)
 	{
-		$this->phpbb_path_helper = $phpbb_path_helper;
-		$this->phpbb_extension_manager = $phpbb_extension_manager;
-		$this->php_ext = $php_ext;
-		$this->phpbb_container = $phpbb_container;
-		$this->db = $db;
+		$this->phpbb_path_helper		= $phpbb_path_helper;
+		$this->phpbb_extension_manager	= $phpbb_extension_manager;
+		$this->php_ext					= $php_ext;
+		$this->phpbb_container			= $phpbb_container;
+		$this->db						= $db;
 		$this->config					= $config;
 		$this->helper					= $helper;
 		$this->auth						= $auth;
-		$this->template = $template;
+		$this->template					= $template;
 		$this->user						= $user;
 	}
 
@@ -95,34 +95,21 @@ class main_listener implements EventSubscriberInterface
 		$event['lang_set_ext'] = $lang_set_ext;
 	}
 
+	public function cr_link_with_id($matches)
+	{
+		return '><span style class="cr4me-link"><img src="' . $ext_path.'images/cr_no_gd.png">'.$matches['id'].'</span><';
+	}
+
 	public function cr4_to_image($event)
 	{
 
 		$text = $event['text'];
-		// Define the ext path. We will use it later for assigning the correct path to our local images
-		$ext_path =
-		$this->phpbb_path_helper->update_web_root_path($this->phpbb_extension_manager->get_extension_path('un1matr1x/ogame', true));
 
 		//Replace CR-Links with nicer Images
-		$search_kb = array();
-		$search_kb[0]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?show=([0-9]+)&amp;pw=[a-zA-Z0-9]{6}&amp;lang=[a-z_]{2,11}<#'; //?show= &pw= &lang=
-		$search_kb[1]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?show=([0-9]+)&amp;lang=[a-z_]{2,11}&amp;pw=[a-zA-Z0-9]{6}<#'; //?show= &lang= &pw=
-		$search_kb[11]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?show=([0-9]+)&amp;lang=[a-z_]{2,11}&amp;pw=<#'; //?show= &lang= &pw=
-		$search_kb[2]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?pw=[a-zA-Z0-9]{6}&amp;show=([0-9]+)&amp;lang=[a-z_]{2,11}<#'; //?pw= &show= &lang=
-		$search_kb[3]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?pw=[a-zA-Z0-9]{6}&amp;lang=[a-z_]{2,11}&amp;show=([0-9]+)<#'; //?pw= &lang= &show=
-		$search_kb[12]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?lang=[a-z_]{2,11}&amp;show=([0-9]+)&amp;pw=[a-zA-Z0-9]{6}<#'; //?lang= &show= &pw=
-		$search_kb[4]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?lang=[a-z_]{2,11}&amp;show=([0-9]+)&amp;pw=<#'; //?lang= &show= &pw=
-		$search_kb[5]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?lang=[a-z_]{2,11}&amp;pw=[a-zA-Z0-9]{6}&amp;show=([0-9]+)<#'; //?lang= &pw= &show=
-		$search_kb[6]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?show=([0-9]+)&amp;pw=[a-zA-Z0-9]{6}<#'; //show= &pw=
-		$search_kb[10] = '#[^"]http://kb.un1matr1x.de/kb\.php\?lang=[a-z_]{2,11}&amp;show=([0-9]+)<#'; //?lang= &show=
-		$search_kb[7]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?show=([0-9]+)&amp;lang=[a-z_]{2,11}<#'; //show= &lang=
-		$search_kb[8]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?show=([0-9]+)&amp;pw=<#'; //?show= &pw=
-		$search_kb[9]	= '#[^"]http://kb.un1matr1x.de/kb\.php\?show=([0-9]+)<#'; //?show=
 
-		$replace_kb = '><span style class="cr4me-link">
-						<img src="' . $ext_path.'images/cr_no_gd.png">$1</span><';
+		$cr_link_pattern = '/[^\"]http(s)?:\\/\\/(?<url>kb\\.un1matr1x\\.de|cr4\\.me)\\/kb\\.php\\?(?<pa1>lang=[a-z_]{2-11}|show=(?<id>[0-9]+)|pw=([a-zA-Z0-9]{6})?)(&amp;|&)?(?&pa1)?(&amp;|&)?/';
 
-		$text = preg_replace($search_kb, $replace_kb, $text);
+		$text = preg_replace_callback($cr_link_pattern, 'self::cr_link_with_id', $text);
 
 		$event['text'] = $text;
 	}
