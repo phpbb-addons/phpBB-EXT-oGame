9a780a245b3608a143a009d4fcb26a4c92483a3f
Falk Seidel <Un1matr1x@users.noreply.github.com>
2015-01-24 18:05:11 +0100

    Fixing test

--- tests/functional/cr4me_link_test.php
+++ tests/functional/cr4me_link_test.php
@@ -21,16 +21,19 @@ class cr4me_functional_posting_test extends \phpbb_functional_test_case
 
 	public function test_post_new_topic_with_cr4me()
 	{
+		$config = new \phpbb\config\config(array('un1matr1x_ogame_cr_link' => 1));
 		$this->login();
 
 		// Test creating topic with a cr-link
-		$post = $this->create_topic(2, 'CR4Me Test Topic', 'Some text about stuff http://kb.un1matr1x.de/kb.php?show=1 with text.');
+		$post = $this->create_topic(2, 'CR4Me Test Topic',
+				'Some text about stuff http://cr4.me/kb.php?show=1 with text.');
 
 		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
 		$this->assertGreaterThan(0, $crawler->filter('.cr4me-link')->count());
 
 		// Test creating a reply with a cr-link
-		$post2 = $this->create_post(2, $post['topic_id'], 'Re: CR4Me Test Topic', 'Some more text with another cr - [url]http://kb.un1matr1x.de/kb.php?lang=da&show=3369&pw=[/url]');
+		$post2 = $this->create_post(2, $post['topic_id'], 'Re: CR4Me Test Topic',
+				'Some more text with another cr - [url]http://kb.un1matr1x.de/kb.php?lang=da&show=3369&pw=[/url]');
 
 		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
 		$this->assertGreaterThan(1, $crawler->filter('.cr4me-link')->count());
