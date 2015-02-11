d63506ee387e1e76fb71cc889dc9d4d7cf3f8bac
Falk Seidel <Un1matr1x@users.noreply.github.com>
2015-02-11 19:04:57 +0100

    reduce duplicate code

--- tests/functional/cr4me_link_test.php
+++ tests/functional/cr4me_link_test.php
@@ -41,29 +41,20 @@ class cr4me_link_posting_test extends \phpbb_functional_test_case
 		$post    = $this->create_topic(2, 'CR4Me Test Topic',
 				'Some text about stuff http://cr4.me/kb.php?show=1 with text.');
 		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
+
 		$this->assertEquals(1, $crawler->filter('.cr4me-link')->count());
-
-		return $post;
-	}
-
-	/**
-	 * @depends test_cr4me_beautification
-	 * @return	object	$post	the created post for furter tests
-	 */
-	public function test_kbun1matr1x_beautification($post)
-	{
-		$this->login();
 
 		$post2   = $this->create_post(2, $post['topic_id'], 'Re: CR4Me Test Topic',
 				'Some more text with another cr - [url]http://kb.un1matr1x.de/kb.php?lang=da&show=3369&pw=[/url]');
 		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
+
 		$this->assertEquals(2, $crawler->filter('.cr4me-image')->count());
 
 		return $post;
 	}
 
 	/**
-	 * @depends test_kbun1matr1x_beautification
+	 * @depends test_cr4me_beautification
 	 */
 	public function test_without_beautification($post)
 	{
