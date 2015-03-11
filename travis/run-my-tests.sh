096b6dda67ae678d62ab05d4c05f0c968dd5082c
Falk Seidel <Un1matr1x@users.noreply.github.com>
2015-03-11 20:00:00 +0100

    Another try to get a better code coverage view

---
+++ travis/run-my-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DB=$1
+TRAVIS_PHP_VERSION=$2
+
+if [ "$TRAVIS_PHP_VERSION" == "5.6" -a "$DB" == "mysqli" ]
+then
+	mkdir build/coverage
+	phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php --verbose --debug --coverage-clover build/logs/clover.xml
+else
+	phpBB/vendor/bin/phpunit --configuration phpBB/ext/$EXTNAME/travis/phpunit-$DB-travis.xml --bootstrap ./tests/bootstrap.php --verbose --debug
+fi
---
+++ travis/upload-coverage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DB=$1
+TRAVIS_PHP_VERSION=$2
+
+if [ "$TRAVIS_PHP_VERSION" = "5.6" -a "$DB" = "mysqli" ]
+then
+	#upload Coverage to CodeClimate
+	CODECLIMATE_REPO_TOKEN=$CODECLIMATE_REPO_TOKEN /home/travis/build/$BASEDIR/vendor/bin/test-reporter  --stdout > codeclimate.json
+	curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
+	#upload Coverage to Scrutinizer
+	cd ../$BASEDIR
+	wget https://scrutinizer-ci.com/ocular.phar
+	php ocular.phar code-coverage:upload --format=php-clover ../../phpBB3/build/logs/clover.xml
+	#upload Coverage to coveralls.io
+	php /home/travis/build/$BASEDIR/vendor/bin/coveralls -v -c ../$BASEDIR/.coveralls.yml
+fi
