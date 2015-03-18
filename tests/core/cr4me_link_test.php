a460fa39c2f288e244f6537a79c999577faf13cb
Falk Seidel <Un1matr1x@users.noreply.github.com>
2015-03-18 11:47:11 +0100

    fix test so that it not fail anymore

--- tests/core/cr4me_link_test.php
+++ tests/core/cr4me_link_test.php
@@ -24,14 +24,15 @@ class cr4me_link_test extends \phpbb_test_case
 
 	public function test_version_agains_composer()
 	{
-		$config = new \phpbb\config\config(array());
-		$config['un1matr1x_ogame_color'] = '31b0d5';
+		$config                               = new \phpbb\config\config(array());
+		$config['un1matr1x_ogame_color']      = '31b0d5';
 		$config['un1matr1x_ogame_color_font'] = 'ffffff';
 
-		$text       = 'http://cr4.me/kb.php?show=1';
+		$text       = '>http://cr4.me/kb.php?show=1<';
 		$cr4me_link = new \un1matr1x\ogame\core\cr4me_link($config);
 		$output     = $cr4me_link->cr4_to_image($text);
 
-		$this->assertEquals('><span class="cr4me-link"><span class="cr4me-link-image ogame_crforme-icon"></span>'.$cr_id.'</span><', $output);
+		$this->assertEquals('><span class="cr4me-link"><span class="cr4me-link-image ogame_crforme-icon">'
+							.'</span>1</span><', $output);
 	}
 }
