8a229e82ac8d5191289f8832f0f2e335fa2e84ba
Falk Seidel <Un1matr1x@users.noreply.github.com>
2015-03-18 12:37:04 +0100

    try to make the test fail again

--- tests/core/cr4me_link_test.php
+++ tests/core/cr4me_link_test.php
@@ -22,14 +22,13 @@ class cr4me_link_test extends \phpbb_test_case
 		return array('un1matr1x/ogame');
 	}
 
-	public function test_version_agains_composer()
+	public function test_link_recognition()
 	{
 		$config                               = new \phpbb\config\config(array());
-		$config['un1matr1x_ogame_color']      = '31b0d5';
-		$config['un1matr1x_ogame_color_font'] = 'ffffff';
-
 		$text       = '>http://cr4.me/kb.php?show=1<';
 		$cr4me_link = new \un1matr1x\ogame\core\cr4me_link($config);
+		$config['un1matr1x_ogame_color']      = '31b0d5';
+		$config['un1matr1x_ogame_color_font'] = 'ffffff';
 		$output     = $cr4me_link->cr4_to_image($text);
 
 		$this->assertEquals('><span class="cr4me-link"><span class="cr4me-link-image ogame_crforme-icon">'
